764962b2874bb85d7c07c4901df23674c72db088
Flaviu1999 <flaviusef@gmail.com>
2024-12-05 01:30:43 +0200

    Update client.lua

--- fv-anunturi/client.lua
+++ fv-anunturi/client.lua
@@ -16,3 +16,24 @@ AddEventHandler("cnn:NextPost", function(info, tip, numar)
             })
     end  
 end)
+
+
+
+
+-- local function ch_ann(player, choice)
+--     local user_id = vRP.getUserId(player)
+
+--     if user_id ~= nil then
+--         vRP.prompt(player, "Anunt:", "", function(player, msg)
+--             msg = tostring(msg)
+--             if (msg ~= "" and msg ~= nil) then
+--                 TriggerClientEvent('chatMessage', -1,
+--                     "^1" .. GetPlayerName(player) .. "^0(^1" .. user_id .. "^0) ^0a dat un anunt administrativ!")
+--                 local name = GetPlayerName(player)
+--                 SetTimeout(5000, function()
+--                     TriggerClientEvent("cnn:NextPost",-1," msg, "administrativ")
+--                 end)
+--             end
+--         end)
+--     end
+-- end
